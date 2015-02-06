e1c3b60374471ff0e95dcde840afd253c48ec4f6
Stanley <stanleywym@gmail.com>
2015-02-06 15:34:06 +0800

    Customize

--- src/main/java/com/medallia/word2vec/Word2VecExamples.java
+++ src/main/java/com/medallia/word2vec/Word2VecExamples.java
@@ -1,8 +1,22 @@
 package com.medallia.word2vec;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.List;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.logging.Log;
+import org.apache.thrift.TException;
+
 import com.google.common.base.Function;
+import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Lists;
 import com.medallia.word2vec.Searcher.Match;
+import com.medallia.word2vec.Searcher.SemanticDifference;
 import com.medallia.word2vec.Searcher.UnknownWordException;
 import com.medallia.word2vec.Word2VecTrainerBuilder.TrainingProgressListener;
 import com.medallia.word2vec.neuralnetwork.NeuralNetworkType;
@@ -13,16 +27,6 @@ import com.medallia.word2vec.util.Format;
 import com.medallia.word2vec.util.ProfilingTimer;
 import com.medallia.word2vec.util.Strings;
 import com.medallia.word2vec.util.ThriftUtils;
-import org.apache.commons.logging.Log;
-import org.apache.thrift.TException;
-import org.apache.commons.io.FileUtils;
-
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.List;
 
 /** Example usages of {@link Word2VecModel} */
 public class Word2VecExamples {
@@ -31,6 +35,7 @@ public class Word2VecExamples {
 	/** Runs the example */
 	public static void main(String[] args) throws IOException, TException, UnknownWordException, InterruptedException {
 		demoWord();
+//		loadModel();
 	}
 	
 	/** 
@@ -38,7 +43,9 @@ public class Word2VecExamples {
 	 * demo-word.sh example from the open source C implementation
 	 */
 	public static void demoWord() throws IOException, TException, InterruptedException, UnknownWordException {
-		List<String> read = Common.readToList(new File("text8"));
+//		List<String> read = Common.readToList(new File("text8"));
+//		List<String> read = Common.readToList(new File("stackexchange-travel-norm1-phrase1"));
+		List<String> read = Common.readToList(new File("news.2012.en.shuffled-norm1-phrase1"));
 		List<List<String>> partitioned = Lists.transform(read, new Function<String, List<String>>() {
 			@Override
 			public List<String> apply(String input) {
@@ -49,12 +56,15 @@ public class Word2VecExamples {
 		Word2VecModel model = Word2VecModel.trainer()
 				.setMinVocabFrequency(5)
 				.useNumThreads(20)
-				.setWindowSize(8)
+//				.setWindowSize(8)
+				.setWindowSize(10)
 				.type(NeuralNetworkType.CBOW)
-				.setLayerSize(5)
+//				.setLayerSize(5)
+				.setLayerSize(100)
 				.useNegativeSamples(25)
 				.setDownSamplingRate(1e-4)
-				.setNumIterations(1)
+//				.setNumIterations(1)
+				.setNumIterations(15)
 				.setListener(new TrainingProgressListener() {
 					@Override public void update(Stage stage, double progress) {
 						System.out.println(String.format("%s is %.2f%% complete", Format.formatEnum(stage), progress * 100));
@@ -63,7 +73,9 @@ public class Word2VecExamples {
 				.train(partitioned);
 		
 		try (ProfilingTimer timer = ProfilingTimer.create(LOG, "Writing output to file")) {
-			FileUtils.writeStringToFile(new File("text8.model"), ThriftUtils.serializeJson(model.toThrift()));
+//			FileUtils.writeStringToFile(new File("text8.model"), ThriftUtils.serializeJson(model.toThrift()));
+//			FileUtils.writeStringToFile(new File("stackexchange-travel-norm1-phrase1.model"), ThriftUtils.serializeJson(model.toThrift()));
+			FileUtils.writeStringToFile(new File("news.2012.en.shuffled-norm1-phrase1.model"), ThriftUtils.serializeJson(model.toThrift()));
 		}
 		
 		interact(model.forSearch());
@@ -73,7 +85,9 @@ public class Word2VecExamples {
 	public static void loadModel() throws IOException, TException, UnknownWordException {
 		final Word2VecModel model;
 		try (ProfilingTimer timer = ProfilingTimer.create(LOG, "Loading model")) {
-			String json = Common.readFileToString(new File("text8.model"));
+//			String json = Common.readFileToString(new File("text8.model"));
+//			String json = Common.readFileToString(new File("stackexchange-travel-norm1-phrase1.model"));
+			String json = Common.readFileToString(new File("news.2012.en.shuffled-norm1-phrase1.model"));
 			model = Word2VecModel.fromThrift(ThriftUtils.deserializeJson(new Word2VecModelThrift(), json));
 		}
 		interact(model.forSearch());
@@ -121,8 +135,20 @@ public class Word2VecExamples {
 				if (word.equals("EXIT")) {
 					break;
 				}
-				List<Match> matches = searcher.getMatches(word, 20);
+				List<Match> matches = searcher.getMatches(word, 10);
 				System.out.println(Strings.joinObjects("\n", matches));
+				
+				ImmutableList<Double> rawVector = searcher.getRawVector(word);
+				System.out.println("Raw vector size: " + rawVector.size());
+				System.out.println("Raw vector: " + StringUtils.join(rawVector, ", "));
+				
+//				for (Match match : matches) {
+//					SemanticDifference similarity = searcher.similarity(word, match.match());
+//					System.out.println("Similarity between " + word + " and " + match.match() + ": ");
+//					for (Match m2 : similarity.getMatches(match.match(), 3)) {
+//						System.out.println("===" + Strings.joinObjects("\n", matches));
+//					}
+//				}
 			}
 		}
 	}
